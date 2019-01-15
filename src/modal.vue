ee8206082a0b89ebf766c6041691db2777f23228
https://github.com/MatchlessHeroVIP/ssmtest.git <lpz714@outlook.com>
2019-01-15 11:18:56 +0800

    rename modal

--- src/modal.vue
+++ src/modal.vue
@@ -1,16 +1,16 @@
 <template>
   <transition name="modal" mode="out-in">
-    <div class="modal-container" ref="modalContainer" v-if="options">
-      <div class="modal-mark" v-if="options.hasMark"></div>
-      <div class="modal-content" :style="modelStyle">
-        <button class="modal-btn" v-if="options.modelType != 1 && options.hasCloseButton" :style="{background: options.activeBgColor, color: options.activeColor}" @click="closeModal">×</button>
-        <div class="modal-header" v-if="options.modelType != 1 && options.hasHeader" :style="{background: options.activeBgColor, color: options.activeColor}">
+    <div class="t-modal-container" ref="modalContainer" v-if="options">
+      <div class="t-modal-mark" v-if="options.hasMark"></div>
+      <div class="t-modal-content" :style="modelStyle">
+        <button class="t-modal-btn" v-if="options.modelType != 1 && options.hasCloseButton" :style="{background: options.activeBgColor, color: options.activeColor}" @click="closeModal">×</button>
+        <div class="t-modal-header" v-if="options.modelType != 1 && options.hasHeader" :style="{background: options.activeBgColor, color: options.activeColor}">
           <span>{{ options.headerText }}</span>
         </div>
-        <div class="modal-body">
+        <div class="t-modal-body">
           <div ref="bodyTemplate" v-html="options.bodyHtml"></div>
         </div>
-        <div class="modal-footer" v-if="options.modelType != 1 && options.hasFooterButtons">
+        <div class="t-modal-footer" v-if="options.modelType != 1 && options.hasFooterButtons">
           <button @click="commitModal" :style="{background: options.activeBgColor, color: options.activeColor}">{{ options.confirmBtnText }}</button>
           <button @click="closeModal" :style="{background: options.normalBgColor, color: options.normalColor}">{{ options.cancelBtnText }}</button>
         </div>
@@ -23,29 +23,29 @@
   import Vue from 'vue';
   export default {
     mounted() {
-        let windowWidth = document.documentElement.clientWidth;
-        let windowHeight = document.documentElement.clientHeight;
-        let vm = this;
-        setTimeout(function() {
-          let positionLeft = (windowWidth - vm.options.modelWidth) / 2;
-          let positionTop = (windowHeight - vm.options.modelHeight) / 2;
-          vm.confirmBtnStyle = 'background:' + vm.options.confirmButtonBgColor + '; color: ' + vm.options.confirmButtonTextColor;
-          vm.cancelBtnStyle = 'background:' + vm.options.cancelButtonBgColor + '; color: ' + vm.options.cancelButtonTextColor;
-          if(typeof vm.options.bodyHtml == 'object' && typeof vm.options.bodyHtml.render == 'function') {
-              let modalBody = Vue.extend(vm.options.bodyHtml);
-              let modalTemplate = new modalBody();
-              modalTemplate.$mount(vm.$refs.bodyTemplate);
-              modalTemplate.options = vm.options.bodyOptions;
-          }
-          vm.modelStyle = 'width:' + vm.options.modelWidth + 'px; height:' + vm.options.modelHeight + 'px; top:' + positionTop + 'px; left:' + positionLeft + 'px; background:' + vm.options.modelBgColor;
-        },100);
+      let windowWidth = document.documentElement.clientWidth;
+      let windowHeight = document.documentElement.clientHeight;
+      let vm = this;
+      setTimeout(function() {
+        let positionLeft = (windowWidth - vm.options.modelWidth) / 2;
+        let positionTop = (windowHeight - vm.options.modelHeight) / 2;
+        vm.confirmBtnStyle = 'background:' + vm.options.confirmButtonBgColor + '; color: ' + vm.options.confirmButtonTextColor;
+        vm.cancelBtnStyle = 'background:' + vm.options.cancelButtonBgColor + '; color: ' + vm.options.cancelButtonTextColor;
+        if(typeof vm.options.bodyHtml == 'object' && typeof vm.options.bodyHtml.render == 'function') {
+          let modalBody = Vue.extend(vm.options.bodyHtml);
+          let modalTemplate = new modalBody();
+          modalTemplate.$mount(vm.$refs.bodyTemplate);
+          modalTemplate.options = vm.options.bodyOptions;
+        }
+        vm.modelStyle = 'width:' + vm.options.modelWidth + 'px; height:' + vm.options.modelHeight + 'px; top:' + positionTop + 'px; left:' + positionLeft + 'px; background:' + vm.options.modelBgColor;
+      },100);
     },
     data() {
-        return {
-          modelStyle: '',
-          confirmBtnStyle: '',
-          cancelBtnStyle: ''
-        }
+      return {
+        modelStyle: '',
+        confirmBtnStyle: '',
+        cancelBtnStyle: ''
+      }
     },
     props: ['options'],
     methods: {
@@ -81,16 +81,10 @@
       opacity: 1;
     }
   }
-  .modal-container {
-
-  }
   .modal-leave-active {
     opacity: 0;
   }
-  /*.modal-enter, .modal-leave-active {*/
-    /*opacity: 0;*/
-  /*}*/
-  .modal-content {
+  .t-modal-content {
     position: absolute;
     box-shadow: -2px 2px 10px 1px rgba(6, 6, 6, 0.2);
     -webkit-box-shadow: -2px 2px 10px 1px rgba(6, 6, 6, 0.2);
@@ -99,7 +93,7 @@
     transition: all 0.5s;
     animation: showIn .7s;
   }
-  .modal-mark {
+  .t-modal-mark {
     position: absolute;
     width: 100%;
     height: 100%;
@@ -108,7 +102,7 @@
     background: rgba(0,0,0,0.4);
     animation: showMark .7s;
   }
-  .modal-btn {
+  .t-modal-btn {
     position: absolute;
     right: 5px;
     top: 5px;
@@ -123,20 +117,20 @@
     outline: none;
     cursor: pointer;
   }
-  .modal-header {
+  .t-modal-header {
     width: 100%;
     height: 50px;
     line-height: 50px;
     font-size: 16px;
     text-align: center;
   }
-  .modal-body {
+  .t-modal-body {
     height: calc(100% - 90px);
     padding: 20px;
     overflow: hidden;
     overflow-y: auto;
   }
-  .modal-footer {
+  .t-modal-footer {
     width: 100%;
     padding: 10px 0;
     position: absolute;
@@ -144,7 +138,7 @@
     font-size: 16px;
     text-align: center;
   }
-  .modal-footer button {
+  .t-modal-footer button {
     padding: 5px 20px;
     margin: 0 10px;
     border: none;
